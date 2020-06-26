70141ba802dbf5f73eae7286cb0d57df4a7c3f02
Joseph Johnson <jwjbadger@gmail.com>
2020-06-26 07:35:20 -0500

    Add get request to get a single user

--- routes/users.js
+++ routes/users.js
@@ -5,25 +5,28 @@ const User = require('../models/User');
 
 // Routes
 
-router.get('/', (req, res) => {
-  res.send('Nice');
+router.get('/:name', async (req, res) => {
+  try {
+    const user = await User.findOne({ name: req.params.name });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const user = new User({
     name: req.body.name,
     todos: req.body.todos,
   });
 
   // Save to the database
-  user
-    .save()
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
+  try {
+    const savedUser = await user.save();
+    res.status(200).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
