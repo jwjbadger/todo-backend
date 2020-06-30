d0743e8695e562b276d226e43af4e584eb5dc343
Joseph Johnson <jwjbadger@gmail.com>
2020-06-30 13:40:39 -0500

    Add put functionality for projects

--- routes/projects.js
+++ routes/projects.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Project = require('../models/Project');
 
 const verify = require('./verify');
+const { json } = require('body-parser');
 
 // Post a new project to the project list
 router.post('/', verify.verify, async (req, res) => {
@@ -21,7 +22,29 @@ router.post('/', verify.verify, async (req, res) => {
     const newProject = await project.save();
     res.status(200).json(newProject);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json({ err: err });
+  }
+});
+
+// Put a project to update
+router.put('/:_id', verify.verify, async (req, res) => {
+  try {
+    if (verify.isEmptyOrSpaces(req.body.users[0])) {
+      return res.status(400).json({ err: 'Invalid users' });
+    }
+
+    Project.updateOne(
+      { _id: req.params._id },
+      {
+        name: req.body.name,
+        description: req.body.description,
+        users: req.body.users,
+        todos: req.body.todos,
+      }
+    );
+    res.status(200).json(req.body);
+  } catch (err) {
+    res.status(400).json({ err: err });
   }
 });
 
@@ -31,7 +54,7 @@ router.get('/:user', verify.verify, async (req, res) => {
     const projects = await Project.find({ users: req.params.user });
     res.status(200).json(projects);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json({ err: err });
   }
 });
 
